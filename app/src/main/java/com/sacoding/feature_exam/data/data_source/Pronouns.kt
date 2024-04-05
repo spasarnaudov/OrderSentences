ad9242e51303901ac165b359267d03ac211e49e3
Spas Arnaudov <spasarnaudov@gmail.com>
2024-04-05 11:24:02 +0300

    Optimize Subjects

--- app/src/main/java/com/sacoding/feature_exam/data/data_source/Pronouns.kt
+++ app/src/main/java/com/sacoding/feature_exam/data/data_source/Pronouns.kt
@@ -2,43 +2,75 @@ package com.sacoding.feature_exam.data.data_source
 
 object Pronouns {
 
-    val subjectNames = listOf(
-        "John",
-        "Emily",
-    )
+    object Subject {
+        val names = listOf(
+            "John",
+            "Emily",
+        )
 
-    val subjectSingularPronouns = listOf(
-        "I",
-        "you",
-        "he",
-        "she",
-        "it",
-    )
+        val singular = listOf(
+            "I",
+            "you",
+            "he",
+            "she",
+            "it",
+        )
 
-    val subjectPluralPronouns = listOf(
-        "we",
-        "you",
-        "they",
-    )
+        val plural = listOf(
+            "we",
+            "you",
+            "they",
+        )
+    }
 
-    val objectSingularPronouns = listOf(
-        "me",
-        "you",
-        "him",
-        "her",
-        "it",
-    )
-    val objectPluralPronouns = listOf(
-        "us",
-        "you",
-        "them",
-    )
+    object Objects {
+        val singular = listOf(
+            "me",
+            "you",
+            "him",
+            "her",
+            "it",
+        )
 
-    val demonstrativesPronouns = listOf(
+        val plural = listOf(
+            "us",
+            "you",
+            "them",
+        )
+    }
+
+    val demonstratives = listOf(
         "this",     //singular pronoun to talk about people or things near us
         "these",    //plural pronoun to talk about people or things near us
         "that",     //singular pronoun to talk about things that are not near us
         "those",    //plural pronoun to talk about things that are not near us
     )
+
+    /**
+     * We use indefinite pronouns to refer to people or things without saying exactly who or what they are.
+     * We use pronouns ending in -body or -one for people, and pronouns ending in -thing for things.
+     */
+    object Indefinite {
+        val body = listOf(
+            "anybody",
+            "everybody",
+            "nobody",
+            "somebody",
+        )
+
+        val one = listOf(
+            "anyone",
+            "everyone",
+            "no one",
+            "someone",
+        )
+
+        val thing = listOf(
+            "anything",
+            "everything",
+            "nothing",
+            "something",
+        )
+    }
 
 }
