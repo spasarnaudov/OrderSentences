0d032b1f51b9464216ce2a1b7f4a1f11d5e9aa0f
Spas Arnaudov <spasarnaudov@gmail.com>
2024-04-05 11:41:58 +0300

    Added some documentation

--- app/src/main/java/com/sacoding/feature_exam/domain/model/Sentence.kt
+++ app/src/main/java/com/sacoding/feature_exam/domain/model/Sentence.kt
@@ -1,5 +1,9 @@
 package com.sacoding.feature_exam.domain.model
 
+/**
+ * Article Noun Verb Preposition Article Noun
+ *   The   cat  sat      on        the   mat.
+ */
 interface Sentence {
     fun positive(): String
     fun negative(): String
