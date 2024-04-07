c960518db1b98bf1fb9c5ec1a47f6c6eb3bd6b91
Spas Arnaudov <spasarnaudov@gmail.com>
2024-04-07 08:36:11 +0300

    Fixed started screen

--- app/src/main/java/com/sacoding/feature_exam/presentation/Screen.kt
+++ app/src/main/java/com/sacoding/feature_exam/presentation/Screen.kt
@@ -1,7 +1,7 @@
 package com.sacoding.feature_exam.presentation
 
 sealed class Screen(val route: String) {
-    object ExamList : Screen("exam_screen")
+    object ExamList : Screen("exam_list")
     object ExamDetail : Screen("exam_details_screen")
     object ExamScreen : Screen("exam_screen")
 }
