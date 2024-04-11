4162b4b1507570fef021fe48b70e53cee7a7d143
Spas Arnaudov <spasarnaudov@gmail.com>
2024-04-11 12:46:38 +0300

    Added possessive determiner to adjective

--- app/src/main/java/com/sacoding/feature_exam/domain/model/Adjective.kt
+++ app/src/main/java/com/sacoding/feature_exam/domain/model/Adjective.kt
@@ -3,6 +3,7 @@ package com.sacoding.feature_exam.domain.model
 val BLANK_ADJECTIVE = Adjective()
 
 data class Adjective(
+    val possessiveDeterminer: String = "",        //my, your, his, her, its, our, their, John's, Emily's
     val qualityOpinion: String = "",    //delicious, beautiful, good
     val size: String = "",              //tall, short, big
     val age: String = "",               //old, young, new, 20-year-old
@@ -14,14 +15,15 @@ data class Adjective(
 ) {
     fun build(): String {
         return buildString {
-            if (qualityOpinion.isNotBlank())    append(qualityOpinion).append(" ")
-            if (size.isNotBlank())              append(size).append(" ")
-            if (age.isNotBlank())               append(age).append(" ")
-            if (shape.isNotBlank())             append(shape).append(" ")
-            if (color.isNotBlank())             append(color).append(" ")
-            if (origin.isNotBlank())            append(origin).append(" ")
-            if (material.isNotBlank())          append(material).append(" ")
-            if (purpose.isNotBlank())           append(purpose).append(" ")
+            if (possessiveDeterminer.isNotBlank())  append(possessiveDeterminer).append(" ")
+            if (qualityOpinion.isNotBlank())        append(qualityOpinion).append(" ")
+            if (size.isNotBlank())                  append(size).append(" ")
+            if (age.isNotBlank())                   append(age).append(" ")
+            if (shape.isNotBlank())                 append(shape).append(" ")
+            if (color.isNotBlank())                 append(color).append(" ")
+            if (origin.isNotBlank())                append(origin).append(" ")
+            if (material.isNotBlank())              append(material).append(" ")
+            if (purpose.isNotBlank())               append(purpose)
         }.trim()
     }
 
--- app/src/test/java/com/sacoding/feature_exam/domain/model/AdjectiveTest.kt
+++ app/src/test/java/com/sacoding/feature_exam/domain/model/AdjectiveTest.kt
@@ -6,81 +6,43 @@ class AdjectiveTest {
 
     @Test
     fun testBuild() {
-        //It's a great big old round white Korean plastic coffee cup.
-        val adjective1 = Adjective(
-            qualityOpinion = "great"
-        ).build()
-        println(adjective1)
-        assert(adjective1 == "great")
+        //It's a my great big old round white Korean plastic coffee cup.
+        val adjective0 = Adjective(
+            possessiveDeterminer = "my"
+        )
+        println(adjective0.build())
+        assert(adjective0.build() == "my")
 
-        val adjective2 = Adjective(
-            qualityOpinion = "great",
-            size = "big"
-        ).build()
-        println(adjective2)
-        assert(adjective2 == "great big")
+        val adjective1 = adjective0.copy(qualityOpinion = "great")
+        println(adjective1.build())
+        assert(adjective1.build() == "my great")
 
-        val adjective3 = Adjective(
-            qualityOpinion = "great",
-            size = "big",
-            age = "old"
-        ).build()
-        println(adjective3)
-        assert(adjective3 == "great big old")
+        val adjective2 = adjective1.copy(size = "big")
+        println(adjective2.build())
+        assert(adjective2.build() == "my great big")
 
-        val adjective4 = Adjective(
-            qualityOpinion = "great",
-            size = "big",
-            age = "old",
-            shape = "round"
-        ).build()
-        println(adjective4)
-        assert(adjective4 == "great big old round")
+        val adjective3 = adjective2.copy(age = "old")
+        println(adjective3.build())
+        assert(adjective3.build() == "my great big old")
 
-        val adjective5 = Adjective(
-            qualityOpinion = "great",
-            size = "big",
-            age = "old",
-            shape = "round",
-            color = "white"
-        ).build()
-        println(adjective5)
-        assert(adjective5 == "great big old round white")
+        val adjective4 = adjective3.copy(shape = "round")
+        println(adjective4.build())
+        assert(adjective4.build() == "my great big old round")
 
-        val adjective6 = Adjective(
-            qualityOpinion = "great",
-            size = "big",
-            age = "old",
-            shape = "round",
-            color = "white",
-            origin = "Korean"
-        ).build()
-        println(adjective6)
-        assert(adjective6 == "great big old round white Korean")
+        val adjective5 = adjective4.copy(color = "white")
+        println(adjective5.build())
+        assert(adjective5.build() == "my great big old round white")
 
-        val adjective7 = Adjective(
-            qualityOpinion = "great",
-            size = "big",
-            age = "old",
-            shape = "round",
-            color = "white",
-            origin = "Korean",
-            material = "plastic"
-        ).build()
-        println(adjective7)
-        assert(adjective7 == "great big old round white Korean plastic")
+        val adjective6 = adjective5.copy(origin = "Korean")
+        println(adjective6.build())
+        assert(adjective6.build() == "my great big old round white Korean")
 
-        val adjective8 = Adjective(
-            qualityOpinion = "great",
-            size = "big",
-            age = "old",
-            shape = "round",
-            color = "white",
-            origin = "Korean",
-            material = "plastic",
-            purpose = "coffee"
-        ).build()
-        println(adjective8)
-        assert(adjective8 == "great big old round white Korean plastic coffee")
+        val adjective7 = adjective6.copy(material = "plastic")
+        println(adjective7.build())
+        assert(adjective7.build() == "my great big old round white Korean plastic")
+
+        val adjective8 = adjective7.copy(purpose = "coffee")
+        println(adjective8.build())
+        assert(adjective8.build() == "my great big old round white Korean plastic coffee")
     }
 }
