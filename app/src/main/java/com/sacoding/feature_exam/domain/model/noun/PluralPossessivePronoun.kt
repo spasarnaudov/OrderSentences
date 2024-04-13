470c41a0660a02b55a799b6bdd3b88f21b716231
Spas Arnaudov <spasarnaudov@gmail.com>
2024-04-13 10:27:23 +0300

    Added PluralPossessivePronoun

--- app/src/main/java/com/sacoding/feature_exam/domain/model/noun/PluralPossessivePronoun.kt
+++ app/src/main/java/com/sacoding/feature_exam/domain/model/noun/PluralPossessivePronoun.kt
@@ -1,14 +1,13 @@
 package com.sacoding.feature_exam.domain.model.noun
 
-class SingularPossessivePronoun(
+class PluralPossessivePronoun(
     private val value: String
 ) : Pronoun(value = value) {
 
     companion object {
-        val MINE = SingularPossessivePronoun("mine")
-        val YOURS = SingularPossessivePronoun("yours")
-        val HIS = SingularPossessivePronoun("his")
-        val HERS = SingularPossessivePronoun("hers")
+        val OURS = PluralPossessivePronoun("ours")
+        val YOURS = PluralPossessivePronoun("yours")
+        val THEIRS = PluralPossessivePronoun("theirs")
     }
 
 }
