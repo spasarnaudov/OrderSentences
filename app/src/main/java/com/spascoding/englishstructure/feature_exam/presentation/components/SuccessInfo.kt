48765e46ee1bb63c2c1e105c8720407e1d3d82f5
Spas Arnaudov <spasarnaudov@gmail.com>
2024-05-09 10:36:49 +0300

    Added content description in success info

--- app/src/main/java/com/spascoding/englishstructure/feature_exam/presentation/components/SuccessInfo.kt
+++ app/src/main/java/com/spascoding/englishstructure/feature_exam/presentation/components/SuccessInfo.kt
@@ -11,6 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.stringResource
+import com.spascoding.englishstructure.R
 import com.spascoding.englishstructure.core.constants.Colors
 import com.spascoding.englishstructure.core.constants.Padding
 
@@ -19,17 +21,19 @@ fun SuccessInfo(success: Boolean) {
     if (success) {
         Image(
             imageVector = Icons.Rounded.Check,
-            modifier = Modifier.padding(Padding.SMALL)
+            modifier = Modifier
+                .padding(Padding.SMALL)
                 .background(Colors.GREEN, CircleShape),
-            contentDescription = "",
+            contentDescription = stringResource(R.string.your_answer_is_correct),
             colorFilter = ColorFilter.tint(Color.White),
         )
     } else {
         Image(
             imageVector = Icons.Rounded.Close,
-            modifier = Modifier.padding(Padding.SMALL)
+            modifier = Modifier
+                .padding(Padding.SMALL)
                 .background(Color.Red, CircleShape),
-            contentDescription = "",
+            contentDescription = stringResource(R.string.your_answer_is_incorrect),
             colorFilter = ColorFilter.tint(Color.White),
         )
     }
