f3543c7ea10882a78d96c0063d5a7155fd37167b
Spas Arnaudov <spasarnaudov@gmail.com>
2024-05-16 12:46:06 +0300

    Added top bar for topics screen

--- app/src/main/java/com/spascoding/englishstructure/feature_exam/presentation/topics_screen/TopicsScreen.kt
+++ app/src/main/java/com/spascoding/englishstructure/feature_exam/presentation/topics_screen/TopicsScreen.kt
@@ -1,56 +1,63 @@
 package com.spascoding.englishstructure.feature_exam.presentation.topics_screen
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.spascoding.englishstructure.core.constants.FontSize
-import com.spascoding.englishstructure.core.constants.Padding
 import com.spascoding.englishstructure.feature_exam.domain.model.getTenseInfo
 import com.spascoding.englishstructure.feature_exam.presentation.Screen
 import com.spascoding.englishstructure.feature_exam.presentation.components.ListElement
 import com.spascoding.englishstructure.feature_exam.presentation.utils.upperFirstLetter
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopicsScreen(
     navController: NavController,
     viewModel: TopicsViewModel = hiltViewModel(),
 ) {
-    val tenseInfoList by viewModel.getTopicInfoFlow().collectAsState(initial = emptyList())
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    vertical = Padding.MEDIUM,
+    val topicInfoList by viewModel.getTopicInfoFlow().collectAsState(initial = emptyList())
+    val topics by viewModel.getTopics().collectAsState(initial = emptyList())
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
                 ),
-            text = viewModel.state.value.tense.value.upperFirstLetter(),
-            fontSize = FontSize.LARGE,
-            textAlign = TextAlign.Center,
-        )
-        val topicInfoList by viewModel.getTopics().collectAsState(initial = emptyList())
+                title = { Text(viewModel.state.value.tense.value.upperFirstLetter()) },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) { innerPadding ->
         LazyVerticalStaggeredGrid(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
             columns = StaggeredGridCells.Fixed(1),
         ) {
-            items(topicInfoList) { topic ->
+            items(topics) { topic ->
                 ListElement(
                     cellText = topic,
-                    progressPercentage = tenseInfoList.getTenseInfo(topic).accuracy.toFloat() / 100f,
+                    progressPercentage = topicInfoList.getTenseInfo(topic).accuracy.toFloat() / 100f,
                     progressColor = viewModel.state.value.tense.color,
                     onClickInfo = {
                         navController.navigate(Screen.TopicDetailScreen.route + "?tense=${viewModel.state.value.tense.int}&topic=$topic")
