8fd7aaeda2fe6ee6ea734744f15c720cde4abb0f
Spas Arnaudov <spasarnaudov@gmail.com>
2024-05-21 14:18:27 +0300

    Made left cell border more thick

--- app/src/main/java/com/spascoding/englishstructure/feature_exam/presentation/components/BorderedListElement.kt
+++ app/src/main/java/com/spascoding/englishstructure/feature_exam/presentation/components/BorderedListElement.kt
@@ -3,16 +3,25 @@ package com.spascoding.englishstructure.feature_exam.presentation.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.spascoding.englishstructure.core.constants.Padding
 import com.spascoding.englishstructure.core.constants.RoundCorner
@@ -22,17 +31,25 @@ fun BorderedListElement(
     onClickItem: (() -> Unit)? = null,
     content: @Composable RowScope.() -> Unit,
 ) {
+    val localDensity = LocalDensity.current
+    var heightIs by remember {
+        mutableStateOf(0.dp)
+    }
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(Padding.MEDIUM)
+            .padding(
+                start = Padding.MEDIUM,
+                bottom = Padding.MEDIUM,
+                end = Padding.MEDIUM,
+            )
             .background(
-                MaterialTheme.colorScheme.primary.copy(alpha = 0.02f),
+                MaterialTheme.colorScheme.primary.copy(alpha = 0.05f),
                 RoundedCornerShape(RoundCorner.MEDIUM)
             )
             .border(
                 1.dp,
-                MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                MaterialTheme.colorScheme.primary,
                 RoundedCornerShape(RoundCorner.MEDIUM)
             )
             .clip(RoundedCornerShape(RoundCorner.MEDIUM))
@@ -41,9 +58,20 @@ fun BorderedListElement(
                 onClick = {
                     onClickItem?.invoke()
                 }
-            ),
+            )
+            .onGloballyPositioned { coordinates ->
+                heightIs = with(localDensity) { coordinates.size.height.toDp() }
+            },
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Box(
+            modifier = Modifier
+                .height(heightIs)
+                .width(Padding.SMALL)
+                .background(
+                    MaterialTheme.colorScheme.primary,
+                )
+        )
         content.invoke(this)
     }
 }
